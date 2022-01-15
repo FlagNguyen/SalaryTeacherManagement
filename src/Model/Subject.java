a2df34c10c4f2f119ac83b96744d67dbc874164a
KyKhoi <kynqhe150376@fpt.edu.vn>
2022-01-15 20:05:20 +0700

    Main

---
+++ src/Model/Subject.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author asus
+ */
+public class Subject {
+    private int subject_Id;
+    private String subject_NameString;
+    private int total_Lesson;
+    private int total_Theory;
+    private int cost_Theory;
+
+    public Subject() {
+    }
+
+    public Subject(int subject_Id, String subject_NameString, int total_Lesson, int total_Theory, int cost_Theory) {
+        this.subject_Id = subject_Id;
+        this.subject_NameString = subject_NameString;
+        this.total_Lesson = total_Lesson;
+        this.total_Theory = total_Theory;
+        this.cost_Theory = cost_Theory;
+    }
+
+    public int getSubject_Id() {
+        return subject_Id;
+    }
+
+    public void setSubject_Id(int subject_Id) {
+        this.subject_Id = subject_Id;
+    }
+
+    public String getSubject_NameString() {
+        return subject_NameString;
+    }
+
+    public void setSubject_NameString(String subject_NameString) {
+        this.subject_NameString = subject_NameString;
+    }
+
+    public int getTotal_Lesson() {
+        return total_Lesson;
+    }
+
+    public void setTotal_Lesson(int total_Lesson) {
+        this.total_Lesson = total_Lesson;
+    }
+
+    public int getTotal_Theory() {
+        return total_Theory;
+    }
+
+    public void setTotal_Theory(int total_Theory) {
+        this.total_Theory = total_Theory;
+    }
+
+    public int getCost_Theory() {
+        return cost_Theory;
+    }
+
+    public void setCost_Theory(int cost_Theory) {
+        this.cost_Theory = cost_Theory;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" + "subject_Id=" + subject_Id + ", subject_NameString=" + subject_NameString + ", total_Lesson=" + total_Lesson + ", total_Theory=" + total_Theory + ", cost_Theory=" + cost_Theory + '}';
+    }
+    
+    
+    
+}
---
+++ src/Model/Teacher.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author asus
+ */
+public class Teacher extends Person {
+
+    private int id;
+    private int level;
+
+    public Teacher(int id, String name, String address, String phone, int level) {
+        super(name, address, phone);
+        this.id = id;
+        this.level = level;
+    }
+
+    public Teacher(int id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    public Teacher() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    
+    
+}
